e77feca4d8ed151ca9dfc1e909527064d97ef480
angelgomfoj <angel.gomfoj@educa.jcyl.es>
2022-10-04 17:15:57 +0200

    simple calculator finished

--- SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ SimpleCalculator/app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -11,11 +11,11 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText num1, num2;
-    Button button;
-    RadioGroup radioGroup;
-    RadioButton suma, resta, multiplicacion, division;
-    TextView result;
+    private EditText num1, num2;
+    private Button button;
+    private RadioGroup radioGroup;
+    private RadioButton suma, resta, multiplicacion, division;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +31,21 @@ public class MainActivity extends AppCompatActivity {
         division = (RadioButton) findViewById(R.id.division);
         result = (TextView) findViewById(R.id.result);
     }
-    public void onClick(View view) {
+    public void calculate(View view) {
         int valor1 = Integer.parseInt(num1.getText().toString());
         int valor2 = Integer.parseInt(num2.getText().toString());
-        int resultadox;
-        if(suma.isSelected()){
-            resultadox = valor1+valor2;
-        }else if(resta.isSelected()){
-            resultadox = valor1 - valor2;
-        }else if(multiplicacion.isSelected()){
-            resultadox = valor1 * valor2;
-        }else if(division.isSelected()){
-            resultadox = valor1 / valor2;
+        int resultado;
+        if(suma.isChecked()){
+            resultado = valor1+valor2;
+        }else if(resta.isChecked()){
+            resultado = valor1-valor2;
+        }else if(multiplicacion.isChecked()){
+            resultado = valor1*valor2;
+        }else if(division.isChecked()){
+            resultado = valor1/valor2;
         }else{
-            resultadox = Integer.MIN_VALUE;
+            resultado = Integer.MIN_VALUE;
         }
-        result.setText(resultadox);
+        result.setText(Integer.toString(resultado));
     }
 }
